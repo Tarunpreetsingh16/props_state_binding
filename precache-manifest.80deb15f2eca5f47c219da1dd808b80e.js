21de3ce6188e1f794b8d1fd7ffedda3f26a0fa88
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-09-23 01:35:01 -0400

    Fixed blank screen from build by telling webpack to fetch things from build folder rather than root folder

---
+++ precache-manifest.80deb15f2eca5f47c219da1dd808b80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e0f26bec01be359f9a582f9cc80fad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32aae64d1e8556e5648c",
+    "url": "./static/css/main.560a5ee8.chunk.css"
+  },
+  {
+    "revision": "2afb2bbe9cab59c83486",
+    "url": "./static/js/2.c1413cd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c1413cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32aae64d1e8556e5648c",
+    "url": "./static/js/main.ff358072.chunk.js"
+  },
+  {
+    "revision": "03ce51522c934d15114b",
+    "url": "./static/js/runtime-main.e367afc1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.589215446ef940c1ddb083963769c526.js"
+  "./precache-manifest.80deb15f2eca5f47c219da1dd808b80e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
